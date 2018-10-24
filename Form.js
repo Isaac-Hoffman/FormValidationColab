a8244aaa65de15a7a3ecf7ff245f48479bc90ec3
HOFFMAN <19hoffmani@gmail.com>
2018-10-24 09:58:00 -0500

    inclass

--- Form.js
+++ Form.js
@@ -1,51 +1,56 @@
 function checkForm(frm){
     var isValid = true;
     var errMessage = "";
-
     //name field
-    if (frm.Name1.value == ""){
+    if (frm.Name.value == "" || frm.Name.value.length < 6){
         isValid = false;
-        frm.Name1.focus();
-        frm.Name1.style.backgroundColor = "#ffccff";
+        frm.Name.focus();
+        frm.Name.style.backgroundColor = "#f76067";
         var name = document.getElementById("errName");
         name.style.display = "inline";
         name.innerHTML = "* required fields are missing";
     }
-
+    else{
+        var name = document.getElementById("errPhone");
+        name.innerHTML = "";
+    }
     //email field
-    if (frm.email.value == "" || frm.email.value <= 3){
+    if (frm.email.value == "" || /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/.test(frm.email.value) == false){
          isValid = false;
          frm.email.focus();
-         frm.email.style.backgroundColor = "#ffccff";
-         var name = document.getElementById("errEmail");
-         name.style.display = "inline";
-         name.innerHTML = "* required fields are missing";
+         frm.email.style.backgroundColor = "#f76067";
+         var nameEmail = document.getElementById("errEmail");
+         nameEmail.style.display = "inline";
+         nameEmail.innerHTML = "* required fields are missing";
     }
-    
-    //name validation. Let me know if this part is wrong. Didn't put anything in html page
-    inputs = form.getElementsByClassName('InputClassName');
-    for (index = 0; index < inputs.length; ++index) {
-        if (inputs[index].value==null || inputs[index].value=="" || inputs[index].value.length < 6)
-        {
-            alert("Field must be 6 characters or more");
-                return false;
+    else{
+        var email = document.getElementById("errPhone");
+        email.innerHTML = "";
+    }
+    //check for selected radio
+    var radios = ["One","Two","Three","Four"];
+    var i;
+    var selectRad = "";
+    for (i = 0; i < radios.length; i++) {
+        if (document.getElementById(radios[i]).checked) {
+            selectRad = radios[i]
         }
     }
+    listBoxSelect = document.getElementById("lstBox").selectedIndex;
+    //phone number validation
+    var phoneNumber = document.getElementById("num").value;
 
-    //phone numver validation. Let me know if this part is wrong. Didn't put anything in html page
-    var phoneNumber;
-
-    function isValid(p) {
-    var phoneRe = /^[2-9]\d{2}[2-9]\d{2}\d{4}$/;
-    var digits = p.replace(/\D/g, "");
-        return phoneRe.test(digits);
+    if (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(phoneNumber)){
+        var phone = document.getElementById("errPhone");
+        phone.innerHTML = "";
     }
-
+    else{
+    isValid = false;
+    frm.PhoneNum.style.backgroundColor = "#f76067";
+    var phone = document.getElementById("errPhone");
+    phone.style.display = "inline";
+    phone.innerHTML = "* required fields are missing";
+    }
     //verifies fields meet the required input
-    if (!isValid){
-     return isValid;
-    } 
-    
+    return isValid;
  }
- 
-        
